27713857b08dcfaf97a3cfd5be5f6552cd057906
rdsilver <rdsilver@student.umass.edu>
2013-08-02 18:36:26 -0400

    Major Look Changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,28 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+var rgb = [250,2,250]
+var dir = [-5,5,-5]
+
+window.onload = function (){
+	changeGlowOfHeader();
+	setInterval(changeGlowOfHeader,600)
+}
+
+function changeGlowOfHeader(){
+
+	for(var x=0;x<3;x++)
+	{
+		rgb[x]+=dir[x]
+		if(!(rgb[x]<255 && rgb[x]>0))
+			dir[x] *=-1;
+		if(Math.random()>.99 && rgb[x]<255 && rgb[x]>0)
+			dir[x] *=-1;
+	}
+
+	$(".nice_heading").css('text-shadow' , '0 0 10px rgb('+ rgb[0] + ',' +rgb[1]+','+rgb[2]+')');
+	$('hr').css('background-color', 'rgb('+ rgb[0] + ',' +rgb[1]+','+rgb[2]+')');
+	$(".glow").css('text-shadow' , '0 0 10px rgb('+ rgb[0] + ',' +rgb[1]+','+rgb[2]+')');
+	$('#sidebar_left').css('background-color', 'rgb('+ rgb[0] + ',' +rgb[1]+','+rgb[2]+')');
+	$('#sidebar_right').css('background-color', 'rgb('+ rgb[0] + ',' +rgb[1]+','+rgb[2]+')');
+}
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
 
   def show
-  	render "show"  
+  	rand_puzzle_id = Puzzle.all.sample.id
+  	redirect_to :controller => 'puzzles' , :action => 'show' , :id => rand_puzzle_id 
   end
 
 end
